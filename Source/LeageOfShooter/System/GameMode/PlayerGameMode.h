1254b8d6c604848da90c429e0ddd397c57c301ff
namolab <52566853+namolab@users.noreply.github.com>
2021-12-09 23:35:57 +0900

    정리

---
+++ Source/LeageOfShooter/System/GameMode/PlayerGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyGameModeBase.h"
+#include "PlayerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAGEOFSHOOTER_API APlayerGameMode : public AMyGameModeBase
+{
+	GENERATED_BODY()
+	
+};
